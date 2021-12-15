1a04dfb1db63ae655c636616a5a324124c1b2339
bdimpe@gmail <imaginarydevelopment@gmail.com>
2021-12-15 14:40:15 -0500

    published 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,10 +5,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.1.1] - 2021-12-15
+
+### Added
+
 - Focuses on Schema shapes and low level helper code
-
-## [Unreleased]
-
 
 ## [0.1.0] - 2017-03-17
 
@@ -18,4 +19,6 @@ First release
 
 - project created bare
 
+[Unreleased]: https://github.com/ImaginaryDevelopment/ToMEHelper/compare/v0.1.1...HEAD
+[0.1.1]: https://github.com/ImaginaryDevelopment/ToMEHelper/compare/v0.1.0...v0.1.1
 [0.1.0]: https://github.com/user/MyCoolNewLib.git/releases/tag/v0.1.0
--- tests/ToMEHelper.Tests/AssemblyInfo.fs
+++ tests/ToMEHelper.Tests/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("ToMEHelper.Tests")>]
 [<assembly: AssemblyProductAttribute("ToMEHelper")>]
-[<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2017-03-17T00:00:00.0000000")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
+[<assembly: AssemblyVersionAttribute("0.1.1")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-12-15T00:00:00.0000000-05:00")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.1")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.1.1")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","bb8964b54bee133e9af64d316dc2cfee16df7f72")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","6885376fbefb5698b6cb520246ef28a698146c6d")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "ToMEHelper.Tests"
     let [<Literal>] AssemblyProduct = "ToMEHelper"
-    let [<Literal>] AssemblyVersion = "0.1.0"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2017-03-17T00:00:00.0000000"
-    let [<Literal>] AssemblyFileVersion = "0.1.0"
-    let [<Literal>] AssemblyInformationalVersion = "0.1.0"
+    let [<Literal>] AssemblyVersion = "0.1.1"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-12-15T00:00:00.0000000-05:00"
+    let [<Literal>] AssemblyFileVersion = "0.1.1"
+    let [<Literal>] AssemblyInformationalVersion = "0.1.1"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "bb8964b54bee133e9af64d316dc2cfee16df7f72"
+    let [<Literal>] AssemblyMetadata_GitHash = "6885376fbefb5698b6cb520246ef28a698146c6d"

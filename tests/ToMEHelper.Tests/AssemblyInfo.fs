004903907bd42dc17f2ade680651e21bba7f9bf3
bdimpe@gmail <imaginarydevelopment@gmail.com>
2021-12-16 14:39:01 -0500

    push new version

--- tests/ToMEHelper.Tests/AssemblyInfo.fs
+++ tests/ToMEHelper.Tests/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("ToMEHelper.Tests")>]
 [<assembly: AssemblyProductAttribute("ToMEHelper")>]
-[<assembly: AssemblyVersionAttribute("0.1.1")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-12-15T00:00:00.0000000-05:00")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.1")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.1.1")>]
+[<assembly: AssemblyVersionAttribute("0.1.2")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-12-16T00:00:00.0000000-05:00")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.2")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.1.2")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","6885376fbefb5698b6cb520246ef28a698146c6d")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","09c89ffd4c332a90e2de206f17d014e990ec7b88")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "ToMEHelper.Tests"
     let [<Literal>] AssemblyProduct = "ToMEHelper"
-    let [<Literal>] AssemblyVersion = "0.1.1"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-12-15T00:00:00.0000000-05:00"
-    let [<Literal>] AssemblyFileVersion = "0.1.1"
-    let [<Literal>] AssemblyInformationalVersion = "0.1.1"
+    let [<Literal>] AssemblyVersion = "0.1.2"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-12-16T00:00:00.0000000-05:00"
+    let [<Literal>] AssemblyFileVersion = "0.1.2"
+    let [<Literal>] AssemblyInformationalVersion = "0.1.2"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "6885376fbefb5698b6cb520246ef28a698146c6d"
+    let [<Literal>] AssemblyMetadata_GitHash = "09c89ffd4c332a90e2de206f17d014e990ec7b88"
